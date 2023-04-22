29a6f15add9845c759bbf82f52cf81fcedeffafc
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-04-22 02:41:21 -0400

    mas conocimientos

---
+++ tipos/03-format-strings.py
@@ -0,0 +1,11 @@
+nombre = "Joaquin"
+apellido = "Gonzalez"
+nombre_completo = f"{nombre} {apellido}"
+# Cuando se aplica el formatter de strings dentro de las llaves puede llevar cualquier valor. ya sean operaciones como indexar
+# Ejemplo: nombre_completo = f"{nombre[0]} {2+5}" dará como resultado la letra J y la suma de 2 y 5.
+
+# Descomenta para probar el ejemplo
+# nombre_completo = f"{nombre[0]} {2+5}"
+
+
+print(nombre_completo)
