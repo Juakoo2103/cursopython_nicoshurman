d358ff9aaadb1ec2d3d08e94d42305c48f675560
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-04-22 02:44:04 -0400

    nuevos conocimientos

--- tipos/04-metodos-strings.py
+++ tipos/04-metodos-strings.py
@@ -1,3 +1,15 @@
-animal = "chanchito feliz"
+animal = "chanCHito feliz"
 
+# Metodos de manejos de strings
+
+# Todo en mayusculas
 print(animal.upper())
+
+# Todo en minusculas
+print(animal.lower())
+
+# Toma la primera letra de la cadena y la deja en mayusculas
+print(animal.capitalize())
+
+# Toma la primera letra de cada palabra dentro del string y la deja en mayusculas
+print(animal.title())
