b6673a6e847d1faf5611fcf830413260baa451f1
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-05-01 02:02:36 -0400

    cambios

--- tipo-avanzados/02-manipulando-listas.py
+++ tipo-avanzados/02-manipulando-listas.py
@@ -8,6 +8,9 @@ mascotas[0] = "Bicho"  # modifica el valor de la posicion 0
 # print(mascotas[::2])  # se salta cada 2 veces
 
 numeros = list(range(21))
-print(numeros[1::2]) #numeros impares 
+print(numeros[1::2]) #numeros impares -1  
+print(numeros[::2]) #numeros pares -1
+print(numeros[:2]) #toma el valor por defecto en el inicio y llega hasta el valor -1
+print(numeros[0:]) #toma el valor por defecto al final y llega hasta el ultimo valor -1
 
 
