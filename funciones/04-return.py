8b7af5417ac976b965ec2e37e0f8b446880c35da
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-05-04 20:23:37 -0400

    aclaracion

--- funciones/04-return.py
+++ funciones/04-return.py
@@ -3,6 +3,7 @@ def suma(a, b):
     return resultado
 
 
+# Aca  al retornar el valor lo puedo guardar como la variable C y la puedo utilizar abajo para realizar otra operacion
 c = suma(10, 20)
 d = suma(c, 20)
 
